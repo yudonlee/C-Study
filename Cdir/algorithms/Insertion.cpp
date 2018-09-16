9cb081cf32085bb6191074da11b88f353640b477
Yudon lee <future0159@naver.com>
2018-09-17 01:10:11 +0900

    modified3

--- Cdir/algorithms/Insertion.cpp
+++ Cdir/algorithms/Insertion.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 using namespace std;
-void insertionSort(int *ptr,int N) {
-  int j,p,Tmp;
-  for(int p=1;p<N;p++ ){
-    Tmp=ptr[p];
-    for(j=p;j>0&&ptr[j-1]<Tmp;j--)
-      ptr[j]=ptr[j-1];
-    ptr[j]=Tmp;
-  }
-}
 int main(){
   int n;
   cin>>n;
   int* tmp = new int[n];
   for(int i=0;i<n;i++)
     cin>>tmp[i];
-  insertionSort(tmp,n);
+  for(int i=1;i<n;i++){
+    int temp =tmp[i];
+    int j=i-1;
+    while(temp>tmp[j] && j>=0){
+      tmp[j+1] =tmp[j];
+      --j;
+    }
+    tmp[j+1]=temp;
+  }
   for(int j=0;j<n;j++)
     cout<<tmp[j]<<endl;
   delete(tmp);
   return 0;
-
 }
