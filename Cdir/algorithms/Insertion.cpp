7d822374924e13216abd297f9ad9c847bb1a0d1f
Yudon lee <future0159@naver.com>
2018-09-17 14:06:13 +0900

    modifed 4

--- Cdir/algorithms/Insertion.cpp
+++ Cdir/algorithms/Insertion.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 int main(){
   int n;
-  cin>>n;
-  int* tmp = new int[n];
+  scanf("%d",&n);
+  int* tmp = (int *)malloc(sizeof(int)*n);
   for(int i=0;i<n;i++)
-    cin>>tmp[i];
+    scanf("%d",&tmp[i]);
   for(int i=1;i<n;i++){
     int temp =tmp[i];
     int j=i-1;
@@ -16,7 +16,7 @@ int main(){
     tmp[j+1]=temp;
   }
   for(int j=0;j<n;j++)
-    cout<<tmp[j]<<endl;
-  delete(tmp);
+    printf("%d\n",tmp[j]);
+  free(tmp);
   return 0;
 }
